5c119c7bd24033cd22daa017ac4c654466e9ea86
Edward-uri <233290@ids.upchiapas.edu.mx>
2026-06-01 10:58:23 -0600

    DI bien hecho agregado a cada feature

---
+++ lib/features/profile/di/profile_module.dart
@@ -0,0 +1,25 @@
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+import '../../../core/http/api_client.dart';
+import '../data/profile_repository_impl.dart';
+import '../data/remote/profile_api.dart';
+import '../domain/repositories/profile_repository.dart';
+
+/// Cableado de dependencias del feature de perfil.
+class ProfileModule {
+  const ProfileModule._();
+
+  static List<SingleChildWidget> providers() => <SingleChildWidget>[
+        Provider<ProfileApi>(
+          create: (ctx) => ProfileApi(
+            ctx.read<ApiClient>(),
+            ctx.read<http.Client>(),
+          ),
+        ),
+        Provider<ProfileRepository>(
+          create: (ctx) => ProfileRepositoryImpl(ctx.read<ProfileApi>()),
+        ),
+      ];
+}
